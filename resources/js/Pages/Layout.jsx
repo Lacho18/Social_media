f68495c687a0fd268c4f0408a72f290fe7c4af2b
LachezarGenov <121165891+Lacho18@users.noreply.github.com>
2025-02-16 00:36:59 +0200

    Adding SignUp page structure

--- resources/js/Pages/Layout.jsx
+++ resources/js/Pages/Layout.jsx
@@ -1,7 +1,9 @@
 export default function Layout({ children }) {
     return (
         <div class="bg-gradient-to-r from-gray-900 via-blue-950 to-black min-h-screen w-screen flex justify-center items-center">
-            {children}
+            <div class="bg-white/10 backdrop-blur-lg p-6 rounded-lg border border-blue-500 shadow-lg w-96 text-center">
+                {children}
+            </div>
         </div>
     );
 }
--- resources/js/Pages/SignUp.jsx
+++ resources/js/Pages/SignUp.jsx
@@ -3,7 +3,99 @@ import Layout from "./Layout";
 export default function SignUp() {
     return (
         <Layout>
-            <h1>Test message</h1>
+            <form className="flex flex-col gap-3">
+                <div className="flex flex-col">
+                    <label for="email" className="text-left text-gray-300 mb-1">
+                        Enter your <span className="font-bold">email</span>
+                    </label>
+                    <input
+                        type="text"
+                        className="p-2 rounder bg-blue-200"
+                        name="email"
+                        placeholder="Email"
+                    />
+                </div>
+
+                <div className="flex flex-col">
+                    <label
+                        for="firstName"
+                        className="text-left text-gray-300 mb-1"
+                    >
+                        Enter your <span className="font-bold">first name</span>
+                    </label>
+                    <input
+                        type="text"
+                        className="p-2 rounder bg-blue-200"
+                        name="firstName"
+                        placeholder="First name"
+                    />
+                </div>
+
+                <div className="flex flex-col">
+                    <label
+                        for="lastName"
+                        className="text-left text-gray-300 mb-1"
+                    >
+                        Enter your <span className="font-bold">last name</span>
+                    </label>
+                    <input
+                        type="text"
+                        className="p-2 rounder bg-blue-200"
+                        name="lastName"
+                        placeholder="Last name"
+                    />
+                </div>
+
+                <div className="flex flex-col">
+                    <label
+                        for="dateOfBirth"
+                        className="text-left text-gray-300 mb-1"
+                    >
+                        Enter your <span className="font-bold">birth date</span>
+                    </label>
+                    <input
+                        type="date"
+                        className="p-2 rounder bg-blue-200"
+                        name="dateOfBirth"
+                    />
+                </div>
+
+                <div className="flex flex-col">
+                    <label
+                        for="password"
+                        className="text-left text-gray-300 mb-1"
+                    >
+                        Enter your <span className="font-bold">password</span>
+                    </label>
+                    <input
+                        type="password"
+                        className="p-2 rounder bg-blue-200"
+                        name="password"
+                        placeholder="Password"
+                    />
+                </div>
+
+                <div className="flex flex-col">
+                    <label
+                        for="password_confirmation"
+                        className="text-left text-gray-300 mb-1"
+                    >
+                        Confirm password
+                    </label>
+                    <input
+                        type="password"
+                        className="p-2 rounder bg-blue-200"
+                        name="password_confirmation"
+                        placeholder="Confirm password"
+                    />
+                </div>
+
+                <input
+                    type="submit"
+                    value="Submit"
+                    class="bg-cyan-400 hover:bg-cyan-500 text-white font-semibold py-2 px-4 rounded shadow-lg mt-4"
+                />
+            </form>
         </Layout>
     );
 }
