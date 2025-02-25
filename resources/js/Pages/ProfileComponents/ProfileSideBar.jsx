36b4de4c9dd0ebb87efc0a09a941e0afcf414676
LachezarGenov <121165891+Lacho18@users.noreply.github.com>
2025-02-25 18:03:15 +0200

    Adding routes path to every recommendation and friend box

--- resources/js/Pages/ProfileComponents/ProfileSideBar.jsx
+++ resources/js/Pages/ProfileComponents/ProfileSideBar.jsx
@@ -1,3 +1,5 @@
+import { Link } from "@inertiajs/react";
+
 export default function ProfileSideBar({
     user,
     addButton,
@@ -9,35 +11,40 @@ export default function ProfileSideBar({
             className="flex justify-between p-2 border-4 border-blue-700 rounded-3xl m-3"
             style={{ borderColor: "#2d2a5c" }}
         >
-            <div className="flex gap-3 items-center">
-                <img
-                    className="w-12 h-12 rounded-full"
-                    src={
-                        user.imagePath !== "null"
-                            ? user.imagePath
-                            : "https://t4.ftcdn.net/jpg/02/15/84/43/360_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg"
-                    }
-                />
-                <div className="flex">
-                    <p className="mr-2">{user.firstName}</p>
-                    <p>{user.lastName}</p>
+            <Link
+                href={`/profilePage/${1}`}
+                className="flex justify-between w-full"
+            >
+                <div className="flex gap-3 items-center">
+                    <img
+                        className="w-12 h-12 rounded-full"
+                        src={
+                            user.imagePath !== "null"
+                                ? user.imagePath
+                                : "https://t4.ftcdn.net/jpg/02/15/84/43/360_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg"
+                        }
+                    />
+                    <div className="flex">
+                        <p className="mr-2">{user.firstName}</p>
+                        <p>{user.lastName}</p>
+                    </div>
                 </div>
-            </div>
-            <div className="flex items-center">
-                {addButton &&
-                    (!sendedRequests.includes(user.id) ? (
-                        <button
-                            className="bg-blue-950 p-2 rounded-xl border-2 border-blue-800"
-                            onClick={() => friendRequestHandler(user.id)}
-                        >
-                            Add friend
-                        </button>
-                    ) : (
-                        <div className="w-10 h-10 rounded-full p-1 bg-green-500 text-center mr-5">
-                            ✔️
-                        </div>
-                    ))}
-            </div>
+                <div className="flex items-center">
+                    {addButton &&
+                        (!sendedRequests.includes(user.id) ? (
+                            <button
+                                className="bg-blue-950 p-2 rounded-xl border-2 border-blue-800"
+                                onClick={() => friendRequestHandler(user.id)}
+                            >
+                                Add friend
+                            </button>
+                        ) : (
+                            <div className="w-10 h-10 rounded-full p-1 bg-green-500 text-center mr-5">
+                                ✔️
+                            </div>
+                        ))}
+                </div>
+            </Link>
         </div>
     );
 }
--- resources/js/Pages/ProfilePage.jsx
+++ resources/js/Pages/ProfilePage.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
+
 export default function ProfilePage() {
     /*
         1. Vish kak da vzemesh parametura ot route
         2. Napravi personalnite stranici
         3. Napravi vruzki kum personalnite stranici kudeto trqbva
     */
+
+    const [user, setUser] = useState(null);
+
+    if (!user) {
+        return (
+            <div className="bg-gradient-to-r from-gray-900 via-blue-950 to-black min-h-screen w-screen flex justify-center items-center text-white">
+                <p>This user does not exist!</p>
+            </div>
+        );
+    }
 
     return (
         <div className="bg-gradient-to-r from-gray-900 via-blue-950 to-black min-h-screen w-screen flex text-white">
